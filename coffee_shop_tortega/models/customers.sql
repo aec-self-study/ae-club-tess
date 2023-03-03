07410b36cd79a325a8433d08aae1ea4cb5c51b30
Tess <51543985+tmo10722@users.noreply.github.com>
2023-03-03 06:40:47 +0000

    remove semicolon

--- coffee_shop_tortega/models/customers.sql
+++ coffee_shop_tortega/models/customers.sql
@@ -6,4 +6,4 @@ select customer_id
 from `analytics-engineers-club.coffee_shop.orders` a
 left join `analytics-engineers-club.coffee_shop.customers` b on a.customer_id = b.id
 group by 1, 2, 3
-order by 4;
+order by 4
