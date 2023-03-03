02c6d97e24e4eb1dba1903464bafca25a4c348b2
Tess <51543985+tmo10722@users.noreply.github.com>
2023-03-03 07:06:39 +0000

    add ref function

--- coffee_shop_tortega/models/monthly_customers.sql
+++ coffee_shop_tortega/models/monthly_customers.sql
@@ -1,6 +1,7 @@
 select date_trunc(first_order_at, month) as date_month,
 count(*) as new_customers
 
-from `dbt_tortega.customers`
+from {{ ref('customers') }}
+`dbt_tortega.customers`
 
 group by 1
