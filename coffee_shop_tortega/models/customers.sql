555d9f92c2f9e214387e565b4c361317f7d68a1e
Tess <51543985+tmo10722@users.noreply.github.com>
2023-03-03 06:38:16 +0000

    add customers.sql

---
+++ coffee_shop_tortega/models/customers.sql
@@ -0,0 +1,9 @@
+select customer_id
+, name
+, email
+, min(a.created_at) as first_order_at
+, count(distinct a.id) as number_of_orders
+from `analytics-engineers-club.coffee_shop.orders` a
+left join `analytics-engineers-club.coffee_shop.customers` b on a.customer_id = b.id
+group by 1, 2, 3
+order by 4;
